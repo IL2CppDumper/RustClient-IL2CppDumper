276e29d470fc022a8fa14a1457e3d5fd509d993f
DarknessSoul <147744208+DarknessSoulYouTube@users.noreply.github.com>
2023-10-15 15:43:20 +0300

    Source Code For RustClient IL2CppDumper

---
+++ npm_inst.bat
@@ -0,0 +1,9 @@
+@echo off
+npm install frida
+pause
+npm install typescript
+pause
+npm install frida-il2cpp-bridge
+pause
+npm install frida-gum
+pause
---
+++ run_rustdumper.bat
@@ -0,0 +1,3 @@
+@echo off
+
+frida "RustClient.exe" -l main.ts
